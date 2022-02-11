458237d647765a4fb5f16c9d773c981acbe7918d
LauTB <45464137+LauTB@users.noreply.github.com>
2022-02-10 21:39:02 -0500

    created data selectors

---
+++ data_selector.py
@@ -0,0 +1,40 @@
+'''
+this functions select a field from the student data
+'''
+
+ci = lambda student: student.ci
+nombre = lambda student: student.nombre
+apellidos = lambda student: student.apellidos
+pais = lambda student: student.pais
+provincia = lambda student: student.provincia
+municipio = lambda student: student.municipio
+situacion_academica = lambda student: student.situacion_academica
+estado = lambda student: student.estado
+direccion = lambda student: student.direccion
+fecha_de_nacimiento = lambda student: student.fecha_de_nacimiento
+grupo = lambda student: student.grupo
+carrera = lambda student: student.carrera
+facultad = lambda student: student.facultad
+tipo_de_curso = lambda student: student.tipo_de_curso
+correo = lambda student: student.correo
+fuente_de_ingreso = lambda student: student.fuente_de_ingreso
+origen_academico = lambda student: student.origen_academico
+regimen_de_estudio = lambda student: student.regimen_de_estudio
+natural_de = lambda student: student.natural_de
+telefono = lambda student: student.telefono
+fecha_de_ingreso_a_la_es = lambda student: student.fecha_de_ingreso_a_la_es
+estado_civil = lambda student: student.estado_civil
+organizacion_politica = lambda student: student.organizacion_politica
+fecha_de_ingreso_al_ces = lambda student: student.fecha_de_ingreso_al_ces
+fecha_de_matricula = lambda student: student.fecha_de_matricula
+sexo = lambda student: student.sexo
+color_de_piel = lambda student: student.color_de_piel
+tipo_de_estudiante = lambda student: student.tipo_de_estudiante
+anno_de_estudio = lambda student: student.anno_de_estudio
+centro_de_trabajo = lambda student: student.centro_de_trabajo
+nombre_padre = lambda student: student.nombre_padre
+na_padre = lambda student: student.na_padre
+nombre_madre = lambda student: student.nombre_madre
+na_madre = lambda student: student.na_madre
+tipo_servicio_militar = lambda student: student.tipo_servicio_militar
+edad = lambda student: student.edad
