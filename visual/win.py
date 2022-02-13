2c331d688b223b6eba62eaaa1071ff620311d09c
ycimadevila <yasmincisneros97@gmail.com>
2022-02-13 01:20:36 -0500

    Done!, Readme updated

--- visual/win.py
+++ visual/win.py
@@ -128,7 +128,7 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "Generador de Grupos"))
         self.btn_open.setText(_translate("MainWindow", "Abrir Archivo"))
         self.btn_show.setText(_translate("MainWindow", "Vista Previa"))
         self.label.setText(_translate("MainWindow", "Generador de Grupos"))
