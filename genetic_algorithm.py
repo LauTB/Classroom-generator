fe75f87c6b344c75fb460221b06e3f57d0d36a99
ycimadevila <yasmincisneros97@gmail.com>
2022-03-08 07:46:58 -0500

    -

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -99,7 +99,9 @@ class GeneticAlgorithm:
         return group, init, end
 
 
-    def get_best_cromosome(self, cromo1, cromo2):
+    def get_best_cromosome(self, cromo_1, cromo_2):
+        cromo1, cromo1_pos = cromo_1
+        cromo2, cromo2_pos = cromo_2
         values1 = self.get_value_from_cromosome(cromo1)
         values2 = self.get_value_from_cromosome(cromo2)
 
@@ -128,10 +130,13 @@ class GeneticAlgorithm:
                 comparison_values[1] += 1
             else: comparison_values[0] += 1
         
-        return cromo1 if comparison_values[0] < comparison_values[1] else cromo2
+        return (cromo1, cromo1_pos) if comparison_values[0] < comparison_values[1] else (cromo2, cromo2_pos)
 
             
-    def generate_new_cromosome(self, parent1, parent2):
+    # parent(1/2) ([ids], [[valores de categorias],...,[]])
+    def generate_new_best_cromosome(self, parent_1, parent_2):
+        parent1, parent1_pos = parent_1[1]
+        parent2, parent2_pos = parent_2[1]
         _, cat = rand.choice(self.categories[0] + self.categories[1])
         min1, max1 = self.get_more_unbalance_group_pair(parent1, cat)
         min2, max2 = self.get_more_unbalance_group_pair(parent2, cat)
@@ -141,88 +146,117 @@ class GeneticAlgorithm:
         pos2 = rand.choice(range(self._get_group_(max1)[1], self._get_group_(max1)[2]))
         temp_id1 = parent1[pos1]
         temp_id2 = parent1[pos2]
+
+        temp_id1_pos = parent1_pos[pos1]
+        temp_id2_pos = parent1_pos[pos2]
+
         child1 = [i for i in parent1]
         child1[pos1] = temp_id2
         child1[pos2] = temp_id1
+        
+        child1_pos = [i for i in parent1_pos]
+        child1_pos[pos1] = temp_id2_pos
+        child1_pos[pos2] = temp_id1_pos
 
         # creando el 2do hijo del parent2
         pos1 = rand.choice(range(self._get_group_(min2)[1], self._get_group_(min2)[2]))
         pos2 = rand.choice(range(self._get_group_(max2)[1], self._get_group_(max2)[2]))
+
         temp_id1 = parent1[pos1]
         temp_id2 = parent1[pos2]
-        child2 = [i for i in parent1]
+        temp_id1_pos = parent1_pos[pos1]
+        temp_id2_pos = parent1_pos[pos2]
+
+        child2 = [i for i in parent2]
         child2[pos1] = temp_id2
         child2[pos2] = temp_id1
 
+        child2_pos = [i for i in parent2_pos]
+        child2_pos[pos1] = temp_id2_pos
+        child2_pos[pos2] = temp_id1_pos
+
+
         # seleccionando el mejor cromosoma de los 4
-        temp_best_cromo = self.get_best_cromosome(parent1, parent2)
-        temp_best_cromo = self.get_best_cromosome(temp_best_cromo, child1)
-        temp_best_cromo = self.get_best_cromosome(temp_best_cromo, child2)
+        temp_best_cromo = self.get_best_cromosome((parent1, parent1_pos), (parent2, parent2_pos))
+        temp_best_cromo = self.get_best_cromosome(temp_best_cromo, (child1, child1_pos))
+        temp_best_cromo = self.get_best_cromosome(temp_best_cromo, (child2, child2_pos))
 
         return temp_best_cromo
 
-    def execute(self):
+    def get_final_cromosome(self):
         cromo1 = self.population[0]
+        for cromo in self.population[1:]:
+            cromo1 = self.generate_new_best_cromosome(\
+                (cromo1, self.get_value_from_cromosome(cromo1)), \
+                    (cromo, self.get_value_from_cromosome(cromo)))
+        return cromo1[0]
+
+    def get_groups(self):
+        groups = []
+        cromosome = self.get_final_cromosome()
+        for group in len(self.amount_of_groups):
+            groups.append([item for item in cromosome[(group * self.groups_len): (((group + 1) * self.groups_len) - 1)]])
+        return groups
 
 
 
 
-class Quicksort:
-    def __init__(self, list_):
-        self.quicksort(list_, 0, len(list_) - 1)
+# class Quicksort:
+#     def __init__(self, list_):
+#         self.quicksort(list_, 0, len(list_) - 1)
 
-    def quicksort(self, list_, left, right):
-        if left < right:
-            indiceParticion = self.particion(list_, left, right)
-            self.quicksort(list_, left, indiceParticion)
-            self.quicksort(list_, indiceParticion + 1, right)
+#     def quicksort(self, list_, left, right):
+#         if left < right:
+#             indiceParticion = self.particion(list_, left, right)
+#             self.quicksort(list_, left, indiceParticion)
+#             self.quicksort(list_, indiceParticion + 1, right)
 
-    def particion(self, list_, left, right):
-        pivot = list_[left]
-        while True:
-            # Mientras cada elemento desde la left esté en orden (sea menor que el
-            # pivot) continúa avanzando el índice
-            while list_[left] < pivot:
-                left += 1
+#     def particion(self, list_, left, right):
+#         pivot = list_[left]
+#         while True:
+#             # Mientras cada elemento desde la left esté en orden (sea menor que el
+#             # pivot) continúa avanzando el índice
+#             while list_[left] < pivot:
+#                 left += 1
 
-            # Mientras cada elemento desde la right esté en orden (sea mayor que el
-            # pivot) continúa disminuyendo el índice
-            while list_[right] > pivot:
-                right -= 1
+#             # Mientras cada elemento desde la right esté en orden (sea mayor que el
+#             # pivot) continúa disminuyendo el índice
+#             while list_[right] > pivot:
+#                 right -= 1
 
-            """
-                Si la left es mayor o igual que la right significa que no
-                necesitamos hacer ningún intercambio
-                de variables, pues los elementos ya están en orden (al menos en esta
-                iteración)
-            """
-            if left >= right:
-                # Indicar "en dónde nos quedamos" para poder dividir el list_ de nuevo
-                # y ordenar los demás elementos
-                return right
-            else:
-                # Nota: yo sé que el else no hace falta por el return de arriba, pero así el algoritmo es más claro
-                """
-                    Si las variables quedaron "lejos" (es decir, la left no superó ni
-                    alcanzó a la right)
-                    significa que se detuvieron porque encontraron un valor que no estaba
-                    en orden, así que lo intercambiamos
-                """
-                list_[left], list_[right] = list_[right], list_[left]
-                """
-                    Ya intercambiamos, pero seguimos avanzando los índices
-                """
-                left += 1
-                right -= 1
+#             """
+#                 Si la left es mayor o igual que la right significa que no
+#                 necesitamos hacer ningún intercambio
+#                 de variables, pues los elementos ya están en orden (al menos en esta
+#                 iteración)
+#             """
+#             if left >= right:
+#                 # Indicar "en dónde nos quedamos" para poder dividir el list_ de nuevo
+#                 # y ordenar los demás elementos
+#                 return right
+#             else:
+#                 # Nota: yo sé que el else no hace falta por el return de arriba, pero así el algoritmo es más claro
+#                 """
+#                     Si las variables quedaron "lejos" (es decir, la left no superó ni
+#                     alcanzó a la right)
+#                     significa que se detuvieron porque encontraron un valor que no estaba
+#                     en orden, así que lo intercambiamos
+#                 """
+#                 list_[left], list_[right] = list_[right], list_[left]
+#                 """
+#                     Ya intercambiamos, pero seguimos avanzando los índices
+#                 """
+#                 left += 1
+#                 right -= 1
 
-"""
-Modo de uso:
-"""
+# """
+# Modo de uso:
+# """
 
-list_ = [5, 1, 2, 1, 1, 3, 5, 1, 5, 1, 99, 231, 234, 12, 121,
-           312, 123, 123, 12, 312, 321, 312, 31, 23, 12, 3123, 123, ]
-print("Antes de ordenarlo: ")
-print(list_)
-Quicksort(list_)
-print("Después de ordenarlo: ")
-print(list_)
+# list_ = [5, 1, 2, 1, 1, 3, 5, 1, 5, 1, 99, 231, 234, 12, 121,
+#            312, 123, 123, 12, 312, 321, 312, 31, 23, 12, 3123, 123, ]
+# print("Antes de ordenarlo: ")
+# print(list_)
+# Quicksort(list_)
+# print("Después de ordenarlo: ")
+# print(list_)
